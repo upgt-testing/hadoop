5cdcc2be6f3a8f8c8f126e39ea73680fbe54055c
shuaiwang516 <shuaiwang516@gmail.com>
2025-02-19 16:55:59 -0600

    update run_tests.sh

--- hadoop-hdfs-project/hadoop-hdfs/run_tests.sh
+++ hadoop-hdfs-project/hadoop-hdfs/run_tests.sh
@@ -11,14 +11,24 @@ fi
 
 mkdir -p logs
 
+# get the total number of test cases
+total_tests=$(wc -l < $input_file)
+counter=0
+start_time=$(date)
+
 # Loop through each file name in the list
 while IFS= read -r base_name; do
-    echo mvn -B surefire:test -Dtest=${base_name} -Dupgt.datanode.restart -Dupgt.namenode.restart
+    # print the progress
+    echo "Running test $base_name"
+    echo "Progress: $((++counter))/$total_tests"
+    echo "Time elapsed: $(($(date +%s) - $(date -d "$start_time" +%s))) seconds"
+    
+    echo mvn -B surefire:test -Dtest=${base_name} -Dupgt.namenode.upgrade -Dupgt.datanode.upgrade -Dupgt.start.version=3.4.0 -Dupgt.upgrade.version=3.5.0-SNAPSHOT
     # restart ONLY
     #    mvn -B surefire:test -Dtest=${base_name} -Dupgt.datanode.restart -Dupgt.namenode.restart > logs/${base_name}.log
 
     # upgrade only
-    mvn -B surefire:test -Dtest=${base_name} -Dupgt.namenode.upgrade -Dupgt.datanode.upgrade -Dupgt.start.version=3.4.0 -Dupgt.upgrade.version=3.5.0-SNAPSHOT > logs/${base_name}.log
+    mvn -B surefire:test -Dtest=${base_name} -Dupgt.namenode.upgrade -Dupgt.datanode.upgrade -Dupgt.start.version=3.4.0 -Dupgt.upgrade.version=3.5.0-SNAPSHOT > logs/${base_name}.log 2>&1
     
     # restart and upgrade
     #    mvn -B surefire:test -Dtest=${base_name} -Dupgt.namenode.restart -Dupgt.datanode.restart -Dupgt.namenode.upgrade -Dupgt.datanode.upgrade -Dupgt.start.version=3.4.0 -Dupgt.upgrade.version=3.5.0-SNAPSHOT > logs/${base_name}.log
