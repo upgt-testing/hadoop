4cc1141354fad6d598a416b6da16252f59267dce
shuaiwang516 <shuaiwang516@gmail.com>
2025-09-16 14:39:53 -0500

    fix

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/RMProxy.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/client/RMProxy.java
@@ -168,10 +168,12 @@ public class RMProxy<T> {
       throw new YarnRuntimeException("Invalid default failover provider class" +
           YarnConfiguration.DEFAULT_CLIENT_FAILOVER_PROXY_PROVIDER, e);
     }
-
+    ClassLoader prevClzLoader = conf.getClassLoader();
+    conf.setClassLoader(Thread.currentThread().getContextClassLoader());
     RMFailoverProxyProvider<T> provider = ReflectionUtils.newInstance(
         conf.getClass(YarnConfiguration.CLIENT_FAILOVER_PROXY_PROVIDER,
             defaultProviderClass, RMFailoverProxyProvider.class), conf);
+    conf.setClassLoader(prevClzLoader);
     provider.init(conf, (RMProxy<T>) this, protocol);
     return provider;
   }
