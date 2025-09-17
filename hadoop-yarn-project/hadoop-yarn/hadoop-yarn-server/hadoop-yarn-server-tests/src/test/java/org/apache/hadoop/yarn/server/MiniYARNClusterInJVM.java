aa338362bfca6010ecfcb10cd4a93ef23d66c883
shuaiwang516 <shuaiwang516@gmail.com>
2025-09-17 00:40:00 -0500

    fix

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/src/test/java/org/apache/hadoop/yarn/server/MiniYARNClusterInJVM.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/src/test/java/org/apache/hadoop/yarn/server/MiniYARNClusterInJVM.java
@@ -317,6 +317,7 @@ public class MiniYARNClusterInJVM extends CompositeService {
             }
         });
         resourceManagerInstance.getVersionClassLoader().resetCurrentThreadClassLoader();
+        newConf.setClassLoader(Thread.currentThread().getContextClassLoader());
     }
 
     private synchronized void startResourceManager(final int index) {
