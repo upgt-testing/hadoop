a830983dc83db3dc1b17647250d2685084e2e0bf
shuaiwang516 <shuaiwang516@gmail.com>
2025-09-23 17:41:00 -0500

    update yarn 2.8.5 with new upgt framework design

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/event/DispatcherJVMInterface.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/event/DispatcherJVMInterface.java
@@ -1,6 +1,6 @@
 package org.apache.hadoop.yarn.event;
 
 public interface DispatcherJVMInterface {
-    void register(Class<? extends Enum> eventType, EventHandler handler);
+    //void register(Class<? extends Enum> eventType, EventHandler handler);
 
 }
