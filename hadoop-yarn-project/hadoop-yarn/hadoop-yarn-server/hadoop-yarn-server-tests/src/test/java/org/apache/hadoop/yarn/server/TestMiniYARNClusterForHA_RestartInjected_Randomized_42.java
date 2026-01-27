cf0c094a9cc8e77dcf65c59ad323358cf1a6c48e
shuaiwang516 <shuaiwang516@gmail.com>
2026-01-27 11:40:45 -0600

    add

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/src/test/java/org/apache/hadoop/yarn/server/TestMiniYARNClusterForHA_RestartInjected_Randomized_42.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.yarn.server;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.ha.HAServiceProtocol;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.server.resourcemanager.HATestUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.restarttest.api.RestartFramework;
+import org.restarttest.core.RestartMode;
+
+public class TestMiniYARNClusterForHA_RestartInjected_Randomized_42 {
+
+    private static final String RM1_NODE_ID = "rm1";
+
+    private static final String RM2_NODE_ID = "rm2";
+
+    private static final int RM1_PORT_BASE = 10000;
+
+    private static final int RM2_PORT_BASE = 20000;
+
+    MiniYARNCluster cluster;
+
+    @Before
+    public void setup() throws IOException, InterruptedException {
+        Configuration conf = new YarnConfiguration();
+        conf.setBoolean(YarnConfiguration.AUTO_FAILOVER_ENABLED, false);
+        conf.set(YarnConfiguration.RM_WEBAPP_ADDRESS, "localhost:0");
+        // Enable HA mode with RPC and fixed ports for restart testing
+        conf.setBoolean(YarnConfiguration.RM_HA_ENABLED, true);
+        conf.set(YarnConfiguration.RM_HA_IDS, RM1_NODE_ID + "," + RM2_NODE_ID);
+        HATestUtil.setRpcAddressForRM(RM1_NODE_ID, RM1_PORT_BASE, conf);
+        HATestUtil.setRpcAddressForRM(RM2_NODE_ID, RM2_PORT_BASE, conf);
+        conf.setBoolean(YarnConfiguration.YARN_MINICLUSTER_FIXED_PORTS, true);
+        conf.setBoolean(YarnConfiguration.YARN_MINICLUSTER_USE_RPC, true);
+        cluster = new MiniYARNCluster(TestMiniYARNClusterForHA.class.getName(), 2, 1, 1, 1);
+        cluster.init(conf);
+        cluster.start();
+        assertFalse("RM never turned active", -1 == cluster.getActiveRMIndex());
+    }
+
+    @After
+    public void tearDown() {
+        if (cluster != null) {
+            cluster.stop();
+        }
+    }
+
+    @Test
+    public void testClusterWorks() throws YarnException, InterruptedException {
+        RestartFramework.at("before_nm_connect").on(cluster).restart("resourcemanager").withIndex(0).withMode(RestartMode.GRACEFUL).execute();
+        assertTrue("NMs fail to connect to the RM", cluster.waitForNodeManagersToConnect(5000));
+        RestartFramework.at("after_nm_connect").on(cluster).restart("nodemanager").withIndex(0).withMode(RestartMode.GRACEFUL).execute();
+    }
+}
